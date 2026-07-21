dc38d27b1a8550f8c8a2a052df2c2857f0eff014
psforestal-rgb <psforestal@yahoo.com>
2026-07-20 19:14:01 -0600

    fix(planificacion): incorporar fuente institucional parte 1

---
+++ src/data/planificacion2026Fuente/texto1.js
@@ -0,0 +1,786 @@
+export default String.raw`Pestaña 1
+
+
+DICIEMBRE 2025
+	DOMINGO
+	LUNES
+	MARTES
+	MIÉRCOLES
+	JUEVES
+	VIERNES
+	SÁBADO
+	
+
+	1
+PNTMM 
+nforme de limpieza 
+Ingresan investigadores ALex
+KM atiende turismo
+YE mantenimiento
+
+
+ 
+PNLQ
+LV atiende turismo
+JV mantenimiento
+JB gestiones e informes, bajan JB y LV y MS con tres vehiculos para dejarlos en Tapanti
+ES ingresa mantenimiento
+
+
+PLE
+MS mantenimiento, va a Tapanti llevando los 2 vehiculos
+
+
+PVM
+Mantenimiento
+	 2
+PNTMM 
+KM atiende turismo
+YE mantenimiento
+FC reunión virtual comisión RLCVS
+ 
+
+PNLQ
+KV en gestiones varias atiende turismo
+DT, ES, LV y JV mantenimiento
+JB gestiones varias
+
+
+
+
+PLE
+MS mantenimiento informes
+
+
+PVM
+CC mantenimiento
+
+
+	3
+PNTMM 
+KM atencion de turismo
+YE mantenimiento
+YC sube a PNLQ
+ 
+
+PNLQ
+FC, YC, JB, Reunión en incopesca 10am
+Visita Meryll
+DT atencion de turismo
+ES mantenimiento
+KV gestiones
+Visitan ACLAP, Ronal, Roy, entre otros
+LV va al taller con el 1100 y regresa por la tarde noche
+
+
+
+
+PLE
+MS mantenimiento, sale
+FC pernocta LE
+
+
+
+
+PVM
+CC y MS mantenimiento gestiones varias
+Reunión de la OSR Perez Zeledón ACLAP 
+
+
+	4
+PNTMM 
+FC, KM, YC, YE, ME en Asamblea ACC 
+AA apoya atendiendo turismo, luego sale a LE
+
+PNLQ
+ES, JB, GG, JV, LV, KV, DT en Asamblea ACC. Sale Meryll con Josue a la asamblea.
+MH apoya atiende visitación
+
+
+
+
+PLE
+Asamblea ACC 
+FC va hacia PNLQ y luego regresa en la noche 
+
+
+
+
+PVM
+CC y MS Asamblea ACC
+MH apoya PNLQ 
+	5
+PNTMM 
+KM atiende turismo, planificación viáticos para operativo de fin de año
+AA y YE ingresan por la tarde trayendo postes de LE, YE sale libre
+AA va a Palomo para transporte de obsequios a indígenas en VM
+GG, ME, FC, ES, ingresan provenientes de PNLQ, YC se queda en Cartago
+GG sale de regreso
+
+
+PNLQ
+GG, ES mantenimiento
+YE ordena bodega.
+KV atiende turismo por la mañana
+YC y ME gestiones varias
+MH, CC, MS, FC, YC, ME  en reunión en PNLQ por la tarde sobre el proceso de traspaso de VM
+
+
+PLE
+FC reunión virtual nuevo RLCVS
+AA mantenimiento y carga postes en el vehículo, trae regalos para niños y se los entrega a MH
+
+
+
+
+PVM
+MH, CC, MS, salen a hacer vueltas, luego en reunión en PNLQ por la tarde sobre el proceso de traspaso de VM. MH luego coordina para entrega regalos a niños en VM
+
+	6
+PNTMM 
+FC y ME gestiones varias
+ES mantenimiento, revisión del 660, motosierras.
+YC atiende turismo
+KM gestiones varias
+
+
+
+PNLQ
+LV atiende turismo
+GG y AA patrulla RBCV
+
+
+
+
+PLE
+AA apoya PNLQ
+
+
+
+
+PVM
+MH mantenimiento
+CC sale
+
+	7
+PNTMM 
+ME atencion visitantes
+FC gestiones varias
+ES, KM, YC, mantenimiento de senderos con motosierras
+ 
+
+PNLQ
+GG atiende visitantes
+LV gestiones informes
+
+
+
+
+PLE
+AA mantenimiento, sale  por incidente
+DT pernocta
+
+
+
+
+PVM
+MH mantenimiento sale
+GG pernocta
+	8
+PNTMM 
+FC y YC Reunión administradores 
+ME atencion visitantes
+GG ingresa a Tapanti a revisar cámaras con ES
+
+
+
+PNLQ
+KV-Cita médica y elaboración de informes
+LV atiende turismo
+GG va a Tapanti a revisar cámaras
+
+
+PLE
+AA  sale por incidente
+DT pernocta
+
+
+PVM
+
+
+	9
+PNTMM 
+FC y YC Reunión administradores
+ME atencion visitantes
+GG y ES sale a la sede a llevar parlante,
+
+
+
+
+PNLQ
+JV reunión turismo
+GG sale a la sede a llevar parlantes
+KV atiende turismo
+ES al cuidado del puesto
+
+
+
+
+PLE
+DT pernocta
+sale a llevar parlante al Guarco
+
+
+PVM
+Covirena Alejandro pernocta
+	10
+PNTMM 
+ME Reunión comisión de emergencias en Bomberos paraíso
+FC reunión virtual todo el día CRLVS 
+YC Reunión administradores
+JB gira RT
+
+PNLQ
+ES mantenimiento y sale
+GG va a la sede a recoger uniformes
+JB ingresa informes gira RT
+
+
+PLE
+MS ingresa atención de visado
+
+
+PVM
+ES pernocta
+
+
+	11
+PNTMM 
+ME cambio aceite vehículo, mantenimiento de senderos con YE
+YE ingresa
+YC atiende turismo
+FC reunión virtual todo el día CRLVS 
+ 
+
+PNLQ
+JB gira RT
+KV y DT atienden turismo
+
+
+PLE
+MS mantenimiento
+
+
+
+
+PVM
+ES pernocta
+
+
+	12
+PNTMM 
+FC reunión virtual todo el día CRLVS 
+ME gestión, atiende turismo
+YE mantenimiento
+GG y JV conteo de murciélagos, con grupo
+
+PNLQ
+KV Reunión de EA y CB presencial
+JB gira RT
+DT atiende turismo
+JV ingresa mantenimiento
+GG apoya PNLQ y PNTMM conteo murciélagos
+
+
+PLE
+MS mantenimiento, informes
+
+
+PVM
+CC ingresa mantenimiento
+	13
+PNTMM 
+ME Atencion Turismo y seguimiento a actividades administrativas
+YE mantenimiento va a LE
+GG sale con JV y YE
+
+PNLQ
+JB atiende turismo
+
+
+
+
+PLE
+MS mantenimiento
+
+
+
+
+PVM
+CC mantenimiento
+
+
+	14
+PNTMM 
+ME atiende turismo, sale
+YE llega con MS hace labores de mantenimiento
+ 
+
+PNLQ
+JB atiende turismo
+
+
+PLE
+MS mantenimiento
+
+
+PVM 
+4 investigadores con el funcionario Carlos Viquez
+CC mantenimiento
+
+
+	15
+PNTMM 
+FC reunión CT
+YE atiende turismo 
+YC informes 
+
+PNLQ
+JV atiende turismo mantenimiento
+ES ingresa en la noche
+
+
+
+
+PLE
+MS visado, mantenimiento
+
+
+
+
+PVM 
+4 investigadores con el funcionario Carlos Viquez
+CC mantenimiento
+MSG SANES, inventarios
+ 
+	16
+PNTMM 
+YE atencion turismo 
+YC informes voluntariado pendientes
+FC reunión RLCVS
+ 
+
+
+
+PNLQ
+JV y ES, DT  II campamento de RFG
+KV atiende turismo
+FC pernocta
+
+
+
+
+PLE
+MS inventario con MSG
+
+
+
+
+PVM 
+4 investigadores con el funcionario Carlos Viquez
+CC traslada a MSG a LE
+
+	17
+PNTMM 
+Pernocta pantera y gente de murcielagos 
+YE mantenimiento 
+YC atencion turismo/reunion con RALEight/SANES 
+KM mantenimiento 
+
+
+
+PNLQ
+JV y ES , DT  II  campamento de RFG
+KV atiende turismo
+Pernocta GP 
+
+
+PLE
+MS traslado a la sede para entregar visado y recoger marchamos
+GP mantenimiento 
+
+
+PVM 
+MH organización curso 
+CC sale a cita médica
+
+	18
+PNTMM 
+Practica ICE 10 pax
+YC atención turismo 
+YE practica con el ICE
+KM SEMEC PNTMM
+FC reunion RLCVS, INCOPESCA 11am, covirenas 18.15pm
+
+
+PNLQ
+JV y ES, DT  II campamento de RFG, regresan al puesto por la tarde
+KV atiende turismo/inicia SEMEC 
+
+
+PLE
+MS SEMEC LA
+CC Recoge a GP en PNLQ y patrullan cerros/inicia SEMEC VM
+GP patrulla cerros con CC
+
+
+PVM 
+MH revisión de equipo para capacitación
+CC en LE 
+	19
+PNTMM 
+YE, YC, ME FC reunion PNLQ
+Covirenas
+ 
+
+PNLQ
+JV sale
+ES, DT, KV,   reunion
+JB apoyo a OSLS
+Ciclistica
+
+
+PLE
+MS sale
+CC reunion PNLQ
+GP al cuidado del puesto
+
+
+
+
+PVM 
+MH CUEFA, brigada Cerro de la Muerte
+MS y CC reunion en PNLQ
+CC en LE
+
+
+
+	20
+PNTMM 
+YE, YC,  Diego COVIRENA, Alto tractor Pejivalle
+YC realiza informe del patrullaje  
+ME recoge a YE y YC 
+KM Atencion visitantes
+
+
+
+
+PNLQ
+ES y CC Providencia, RBCV
+JB  atencion turismo 
+ES: Realiza el informe
+ingresa Alex investigador de anfibios con 4p
+
+
+
+
+PLE
+CC patrullaje Providencia con ES, GP
+
+
+PVM 
+MH CUEFA, brigada Cerro de la Muerte
+	21
+PNTMM 
+ME atencion visitantes
+YE y YC y KM mantenimiento patrullaje
+ 
+
+PNLQ
+JB atencion turismo
+
+
+
+
+PLE
+YC, JB, YE y, ES La Esperanza
+ME: Recoge
+JB Realiza el informe
+CC sale
+
+
+
+
+PVM 
+MH CUEFA, brigada Cerro de la Muerte 
+
+
+
+	22
+PNTMM 
+FC gestión
+YC informes
+KM informes
+ME atencion de turismo
+YE mantenimiento sale 
+
+PNLQ
+JB y GP  Ciénagas, Los Calvo, Damita
+GP realiza el informe 
+DT informes
+KV atiende turismo
+
+
+
+
+PLE
+GP patrullaje
+
+
+
+
+
+PVM 
+MH mantenimiento
+ 
+	23
+PNTMM 
+FC gestión apoya PNLQ
+ME atencion turismo
+KM mantenimiento
+ 
+
+PNLQ
+FC apoya PNLQ
+DT informes 
+JB y KV: Los Cerros y Reserva Biológica
+KV. Realiza el informe
+ES mantenimiento
+
+
+PLE
+GP mantenimiento
+
+
+
+
+
+PVM 
+ GP y MH La Damita, Las Duelas, Guayabillos, Macho Gaff
+MH realiza informe
+
+	24
+PNTMM 
+ME atencion turismo
+KM mantenimiento
+ 
+
+PNLQ
+FC gestión y sale
+JB, MH y GP Retén en Carretera 
+MH realiza informe 
+DT atencion turismo
+KV informes
+JV ingresa por la noche
+
+
+
+
+PLE
+GP mantenimiento
+
+
+
+
+
+PVM 
+MH realiza informe
+
+	25
+PNTMM 
+ME atencion turismo
+KM mantenimiento
+ 
+
+PNLQ
+JB, MH y GP RBCV
+GP realiza informe 
+JV atiende turismo
+
+
+
+
+PLE
+GP RBCV
+
+
+
+
+
+PVM 
+MH RBCV
+
+
+
+	26
+PNTMM 
+ME Atencion Turismo A Administrativas
+KM, Mantenimiento
+3 estudiantes TCU 
+ 
+
+PNLQ
+ JV, JB, GP  Los 3000
+JV realiza informe 
+DT mantenimiento
+KV atiende turismo
+
+
+PLE
+GP patrullaje los 3000
+
+
+
+
+
+PVM 
+MH mantenimiento
+
+	27
+PNTMM 
+ME atencion turismo
+KM mantenimiento
+3 estudiantes TCU
+ 
+
+PNLQ
+JV, GP, l Retén 
+Jv realiza el informe
+JB atencion turismo
+
+
+PLE
+MS y GP Reten
+
+
+
+
+
+PVM 
+MH Retén
+CC ingresa manteminiento
+	28
+PNTMM 
+ME atencion turismo y sale
+KM mantenimiento
+3 estudiantes TCU
+ 
+
+PNLQ
+MS y JV Cerros y RBCV
+MS realiza el informe 
+JB atencion turismo
+
+
+
+
+PLE
+MS Cerros y RBCV
+GP sale
+
+
+
+
+PVM 
+CC Cerros y RBCV
+MH sale
+
+
+	29
+PNTMM 
+YC mantenimiento atencion de visitantes
+FC gestiones varias
+3 estudiantes TCU
+ 
+
+PNLQ
+ JV, MS y CC y ES Guayabillos Macho Gaff
+CC realiza el informe 
+DT mantenimiento
+KV atencion rurismo
+
+
+
+
+PLE
+MS Guayabillos Macho Gaff
+
+
+
+
+PVM 
+CC patrullaje
+ 
+	30
+PNTMM 
+YC mantenimiento atencion de visitantes
+FC gestiones varias
+3 estudiantes TCU
+ 
+
+PNLQ
+MS, JV ; ES, CC Providencia
+MS realiza el informe 
+CC apoya PNLQ
+DT mantenimiento
+KV atencion rurism
+
+
+PLE
+MS Providencia
+
+
+
+
+PVM 
+CC apoya PNLQ
+
+	31
+PNTMM 
+YC y covirena Alto tractor, embalse el llano
+YC realiza el informe
+FC gestiones varias atención de visitantes  
+
+PNLQ
+MS, ES CC RBCV
+MS realiza el informe 
+DT mantenimiento
+KV atencion rurism
+
+
+PLE
+MS RBCV
+
+
+
+
+PVM 
+CC en RBCV
+
+	
+
+	
+
+	
+
+
+
+
+ENERO 2026
+	DOMINGO
+	LUNES
+	MARTES
+	MIÉRCOLES
+	JUEVES
+	VIERNES
+`;
