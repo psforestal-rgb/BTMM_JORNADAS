0fa70a36b4d148a9bb5619ada1f0b275f1e9e9bf
psforestal-rgb <psforestal@yahoo.com>
2026-07-20 19:21:56 -0600

    fix(planificacion): incorporar fuente institucional parte 7

---
+++ src/data/planificacion2026Fuente/texto7.js
@@ -0,0 +1,868 @@
+export default String.raw`PNTMM
+FC apoyo PNLQ
+KM atencion de turismo
+
+
+
+
+PNLQ
+FC apoyo PNLQ
+JPG y JV patrullaje Los Calvo
+DT atencion visitantes
+
+
+
+
+
+
+PLE
+MS va a Tapanti a recoger cuadraciclo
+ingresa investigadores UNED
+
+
+
+
+
+
+ 
+	13
+PNTMM
+ES y KM entrenamiento MIF
+FC atencion turismo  Conteo de anfibios
+
+
+
+
+PNLQ
+KV-Actividad Reforestación CBLS-en la UNED
+JV:atención de turismo
+JV Conteo de anfibios 
+
+
+PLE
+MS mantenimiento informes ingresa investigadores UNED 
+
+
+
+
+
+
+
+ 
+	14
+PNTMM
+JV Conteo de anfibios 
+
+
+
+
+PNLQ
+
+
+
+
+
+
+PLE
+
+
+
+
+
+
+
+
+
+ 
+	15
+PNTMM
+ME: Entra seguimiento a sus labores
+
+
+
+
+PNLQ
+KV-PS-Actividad del día del árbol-Municipalidad de Dota
+
+
+
+
+PLE
+
+
+
+
+
+
+
+
+
+ 
+	16
+PNTMM
+reunión proyecto refugios climáticos en PNLQ  10am
+
+
+
+
+PNLQ
+DT:Herramienta de flujo de visitantes
+reunión proyecto refugios climáticos en PNLQ 10am
+
+
+
+
+PLE
+reunión proyecto refugios climáticos en PNLQ 10am
+
+
+
+	17
+PNTMM
+Lavado de vehiculo 347-166
+
+
+
+
+PNLQ
+DT:Herramienta de flujo de visitantes
+
+
+
+
+PLE
+
+
+
+
+
+
+
+
+
+	18
+PNTMM
+KM mantenimiento 
+Me pendientes
+YC denuncia/respuesta a usuaria
+
+
+PNLQ
+DT:Herramienta de flujo de visitantes
+MS audiencia
+ES turismo/viaticos primera quincena
+JPG viáticos I quincena
+KV mantenimiento barajas
+PS mantenimiento barajas
+
+
+
+
+PLE
+
+
+YE mantenimiento
+
+
+ 
+	19
+PNTMM
+FC Comite Tecnico
+Patrullaje YC con Es y km 
+ME varios administrativos 
+
+
+PNLQ
+DT:Herramienta de flujo de visitantes
+JPG  y YE: Reten Macho Gaff con Fuerza Pública
+PS turismo
+Jpg mantenimiento barajas con Iván .
+
+
+PLE
+YE: Reten Macho Gaff con Fuerza Pública.
+
+
+	20
+PNTMM
+ME turismo/voluntwriado 
+Voluntariado 3 pax 
+Es pruebas camada en BA 
+
+
+PNLQ
+
+
+PG turismo 
+
+
+PLE
+
+
+YE y MS patrullaje durante la mañana, esperanza, duelas, etc/tarde informes viáticos etc 
+
+
+	21
+PNTMM
+ME turismo 
+
+
+
+
+PNLQ
+JPG turismo 
+ES apoyo a JPG/recorrido senderos 
+
+
+PLE
+MS:Patrullaje, La Esperanza
+YE::Patrullaje, La Esperanza
+
+
+
+
+
+
+
+	22
+PNTMM
+Reunión teams 
+FC YC capacitación órdenes administrativas
+DEKRA 166
+ME rtv con el 166
+KM capacitación 
+
+
+PNLQ
+KV-Actividad de Educación Ambiental con Amaa
+Reunión teams 
+DEKRA 16
+PS y PG Capacitación órdenes administrativas y my office 
+
+
+PLE
+MS rtv con Errol bajan en 16 
+YE reunión teams virtual 
+Reunión teams 
+GP entra 
+
+
+ 
+	23
+PNTMM
+YC tema zonificacion rio macho 
+ME 347-166 a taller de ajuste de freno de mano.
+KM mantenimiento/decomiso
+ES mantenimiento/decomiso. 
+
+
+PNLQ
+JPG baja 347-16 a taller coordina con Mayra
+PS Inicia valoración de daño con YC, pedir info a josué y Mariano 
+KV informes actividades EA
+DT turismo/reunión con YC sobre herramienta de cahuita
+
+
+
+
+PLE
+
+
+MS baja 1100 a recoger a PG a taller
+YE mantenimiento de puesto
+GP capacitación my office 
+
+
+	24
+PNTMM
+FC Gira PNLQ ASADA
+YC operativo decomiso
+ES operativo decomiso
+KM operativo decomiso
+ME turismo/capacitacion my office/informe voluntarios de finde (cogui)
+
+
+PNLQ
+KV- Taller CBLS con Agathos
+PG capacitacion my office/lleva 347-16 a taller con MS 
+PS capacitacion my office/mantenimiento senderos 
+Dt capacitación my office/herramienta con JV  
+JV capacitacion my office/turismo/herramienta con DT
+Investigador ENRIQUE pernocta en el puesto 
+
+
+PLE
+GP capacitacion my office/mantenimiento barajas con PS 
+
+
+MS capacitacion My office/recoge a JPG en taller 
+YE capacitacion my office/sale
+
+
+ 
+	25
+PNTMM
+ME:Taller Servicio al cliente PVI
+YC:Taller Servicio al cliente PVI
+FC gestion
+KM informes atencion turismo
+
+
+
+PNLQ
+Investigador ENRIQUE pernocta en el puesto no llegó
+JG taller SC en PNVI
+KV atención turismo
+PS informes
+JPG taller vehiculo 16
+DT y ES proyecto MIF en escuelas
+
+
+
+
+PLE
+MS:Taller Servicio al cliente en PVI
+GP y YE  mantenimiento
+
+
+
+
+	26
+PNTMM
+Investigador ENRIQUE pernocta en el puesto no llegó
+YC: reunion sobre los SANES con JV y DT y MS
+FC gestiones varias
+KM mantenimiento informes
+llegan voluntarios
+
+
+
+
+PNLQ
+KV- Reunión del CBLQ
+DT Charla UCR sobre trabajo comunitario 1pm
+JV reunión y cita médica
+PS cita médica, visados 
+JV y JP: Reten Ruta 2, Macho Gaff con Fuerza Pública (cancelado)
+JP mantenimiento
+
+
+
+
+PLE
+MS y GP audiencia en tribunales:
+
+
+	27
+PNTMM
+Investigador ENRIQUE pernocta en el puesto no llegó
+FC gestiones varias
+1 covirena y2 voluntarios
+TCU UCR
+
+
+
+
+PNLQ
+JV y MS:Patrullaje, Reserva B,Sn Gerardo
+GP apoya PNLQ atención visitantes
+JPG mantenimiento de senderos sale libre
+
+
+
+
+PLE
+MS y JV :Patrullaje, Reserva B,Sn Gerardo
+GP apoya PNLQ atención visitantes
+Ingreso del proyecto tigrillo
+
+
+
+
+
+
+ 
+	28
+PNTMM:
+Pasante frances Enzo
+2 voluntarias 
+Pasante frances Enzo
+FC gestiones varias
+
+
+
+
+PNLQ
+JV y MS: Patrullaje cacería 
+GP apoya PNLQ atención visitante
+TCU UCR
+
+
+
+
+PLE: 
+JV y MS: Patrullaje Laguna Berma
+GP apoya PNLQ atención visitante
+Ingreso del proyecto tigrillo
+
+
+ 
+	29
+PNTMM
+Pasante frances Enzo
+2 voluntarias 
+
+
+
+
+
+
+PNLQ
+KV-Actividad de Educación Ambiental con Amaia
+DT gira diplomado
+
+
+PLE
+
+
+
+
+
+
+ 
+	30
+PNTMM
+Pasante frances Enzo
+2 voluntarias 
+Reunion con Camilo 
+
+
+PNLQ
+DT gira diplomado
+
+
+
+
+PLE
+
+
+
+
+
+
+
+ 
+	
+
+
+
+
+ 
+	
+
+	
+
+	
+
+	
+
+	
+
+
+
+
+
+
+
+JULIO 2026
+	DOMINGO
+	LUNES
+	MARTES
+	MIÉRCOLES
+	JUEVES
+	VIERNES
+	SÁBADO
+	3e
+	
+
+	
+
+	1
+PNTMM
+
+
+VB limpieza del BTMM
+Pasante frances Enzo
+Errol:Patrullaje alto araya-Guabata
+KM:Patrullaje alto araya-Guabata
+ME:Retirar las tarjetas institucionales en la sede y pasar al taller a retirar el vehículo 347-16
+
+
+
+
+PNLQ
+DT gira diplomado
+
+
+
+
+PLE
+YE mantenimiento
+
+
+
+
+
+
+ 
+	2
+PNTMM
+VB limpieza del BTMM
+Pasante frances Enzo
+ES coordinación y traslado de materiales de colono la chinchilla a la esperanza con Enoc de tortuguero
+ME: Actividades Administrativas.
+
+
+PNLQ
+DT gira diplomado
+pernocta enoc de tortuguero
+KV atencion turismo
+PS informes varios
+JV mantenimiento sale
+ES pernocta en el puesto
+Pasante Enzo colabora
+
+
+PLE
+YE mantenimiento
+
+
+
+
+
+
+
+ 
+	3
+PNTMM
+Pasante franc
+ME: Actividades Administrativas
+YC: Seguimiento a las actividades administrativas
+KM:Realizar denuncias,viaticos,otros
+ES:Mantenimiento ZUP
+PNLQ
+DT gira diplomado
+Enzo apoya PNLQ
+JPG: Traslado a Tapanti
+
+
+
+
+PLE
+YE mantenimiento
+
+
+
+
+
+
+
+ 
+	4
+PNTMM
+Pasante frances Enzo
+ME y ES:Celebracion Dia del Felino,Trinidad de Dota, pernocta en OLE
+JPG: Atencion de turismo
+
+
+PNLQ
+KV:Celebracion Dia del Felino,Trinidad de Dota 
+LV atencion turismo
+
+
+
+
+PLE
+YE:Celebracion Dia del Felino,Trinidad de Dota 
+
+
+
+
+
+
+ 
+	5
+PNTMM
+Pasante frances Enzo
+ME:Pernocta en OLE. En la tarde baja a Orosi
+PG:Atencion turismo y Cuida Puesto
+
+
+PNLQ
+
+
+
+
+
+
+PLE
+
+
+
+
+
+
+
+
+
+ 
+	6
+PNTMM
+Pasante frances Enzo trabaja con Errol
+ME: Reunion PGAI
+ES: Mantenimiento ZUP, Baterías sanitaria y acueducto.
+YE: Semec y otros actividades Administrativas
+
+
+PNLQ
+KV-Reunión PGAI
+DT gira diplomado
+PG: Apoyo SEMEC sube a quetzales
+
+
+PLE
+
+
+
+
+
+
+ 
+	7
+PNTMM
+Pasante frances Enzo
+YC y FC reunión con P&G 
+ME: pendientes , sitas DEKRA cuadraciclos
+ES: mantenimiento 
+
+
+PNLQ
+DT gira diplomado
+KV:Patrullaje Jaboncillos,Ciénegas Las Duelas
+LV:Patrullaje Jaboncillos,Ciénegas Las Duelas
+JPG atencion turismo/termina semec 
+
+
+PLE
+GP y YE Patrullaje Jaboncillos,Ciénegas Las Duelas
+
+
+
+
+ 
+	8
+PNTMM
+Pasante frances Enzo
+Enzo pinta porton 
+ME:Reunión CME, Informe de ventas
+ES mantenimiento 
+YC PP/Expediente la Georgina/capacitacion My office/inforrmes de ventas PNTMM
+
+
+PNLQ
+KV-Reunión CME
+DT gira diplomado
+LV mantenimiento barajas
+JPG mantenimiento barajas
+
+
+PLE
+YE mantenimiento barajas
+GP atención turismo
+
+
+
+
+
+
+
+ 
+	9
+PNTMM
+ME pendientes/sale
+FC entra
+ME: Reunion INA
+
+
+PNLQ
+DT gira diplomado
+JV:Instalación Cámaras Trampa RB/plan de trabajo de voluntariado PNTMM y PNLQ
+JPG instalación de cámaras trampa en RCV/actualiza semec con lo de memo y Mariano
+LV atención turismo 
+KV instalación cámaras trampa con JV y JPG
+
+
+
+
+PLE
+GP:Revisar internet y electricidad/turismo en PNLQ
+
+
+YE sube con guillermo apoyo PNLQ/sale 
+
+
+
+ 
+	10
+PNTMM
+3 tesistas del TEC
+FC
+
+
+PNLQ
+DT gira diplomado
+LV:Reten Ruta 2
+JV:Patrullaje SAn gerardo/providencia por la mañana y retén en la tarde 
+PG:baja cuadraciclo a Dekra con ES
+KV atencion de turismo
+Dekra 29-862
+
+
+PLE
+GP:Patrullaje san gerardo/providencia por la mañana y Reten Ruta 2
+
+
+
+
+
+
+ 
+	11
+PNTMM
+3 tesistas del TEC
+
+
+PNLQ
+PG:Patrullaje Reserva Biológica
+
+
+
+
+PLE
+GP:Patrullaje Reserva Biológica
+
+
+
+
+
+
+ 
+	12
+PNTMM
+Pasante frances Enzo
+
+
+
+
+PNLQ
+
+
+
+
+
+
+PLE
+Ingresa investigadora de gases de la UNA Luisa Castillo
+
+
+
+
+
+
+
+
+
+ 
+	13
+PNTMM
+Taller de investigación en Monteverde
+Pasante frances Enzo
+YC informes voluntariado
+
+
+
+
+PNLQ
+JV Taller de investigación en Monteverde
+
+
+
+
+PLE
+
+
+
+
+
+
+
+
+
+ 
+	14
+PNTMM
+Taller de investigación en Monteverde
+Pasante frances Enzo
+YC:Juicio contravensional paraiso/PP/SEMEC
+am/Patrullaje,Humo,pejibaye,Cartago
+KM:Patrullaje,Humo,pejibaye,Cartago
+ES:Patrullaje,Humo,pejibaye,Cartago
+PNLQ
+JV Taller de investigación en Monteverde
+
+
+
+
+PLE
+mantenimiento GP
+
+
+
+	15
+PNTMM
+Taller de investigación en Monteverde
+Pasante frances Enzo
+YC PP/SEMEC/DENUNCIA/voluntariado 
+KM PP/SEMEC
+
+
+
+
+PNLQ
+JV Taller de investigación en Monteverde
+ES trabajo con voluntarios 
+
+
+
+
+PLE
+
+
+GP atención turismo en PNLQ
+
+
+
+
+
+ 
+	16
+PNTMM
+FC Comite  Tecnico
+Pasante frances Enzo
+
+
+
+
+PNLQ
+DA pernocta gira de sitada
+`;
