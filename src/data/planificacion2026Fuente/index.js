2484b85674eda1ba7c13e521238856abe0d5e0c5
psforestal-rgb <psforestal@yahoo.com>
2026-07-20 19:25:55 -0600

    fix(planificacion): ensamblar fuente institucional local

---
+++ src/data/planificacion2026Fuente/index.js
@@ -0,0 +1,26 @@
+import texto1 from "./texto1.js";
+import texto2 from "./texto2.js";
+import texto3 from "./texto3.js";
+import texto4 from "./texto4.js";
+import texto5 from "./texto5.js";
+import texto6 from "./texto6.js";
+import texto7 from "./texto7.js";
+import texto8 from "./texto8.js";
+import texto9 from "./texto9.js";
+
+/**
+ * Copia local de la exportación de PLANIFICACION BTMM 2026.
+ * Se incluye en el bundle para que GitHub Pages no dependa de una descarga
+ * directa desde Google Docs, la cual es bloqueada por CORS/autenticación.
+ */
+export const PLANIFICACION_2026_TEXTO = [
+  texto1,
+  texto2,
+  texto3,
+  texto4,
+  texto5,
+  texto6,
+  texto7,
+  texto8,
+  texto9,
+].join("");
